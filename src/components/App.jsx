c0d422c908c2aca2a034957a8b0477a6b67b0858
Arsanyos <arsanyos1600@gmail.com>
2022-02-11 14:05:24 +0300

    - image in navbar working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import Navbar from './Navbar.jsx';
+import React from "react";
+//importing components
+import CompResearch from "./CompResearch.jsx";
+import Navbar from "./Navbar.jsx";
 //importing style
-import '../style/App.css';
-function App(){
-    return(
-        <div className = "app">
-            <Navbar />
-        </div>
-    );
+import "../style/App.css";
+function App() {
+  return (
+    <div className="app">
+      <div className="nav">
+        <Navbar />
+      </div>
+      <div className="app-content"></div>
+    </div>
+  );
 }
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React from "react";
+//importing images
 
-function Navbar(){
-    return(
-        <div className="nav-bar">
-            <div className="aastu-logo">
-                <img src="https://images.app.goo.gl/7qQyQjAUczvoCMx26" width="200px" height="200px" alt="aastu-logo"/>
-                <p>Addis Ababa Science and Technology University</p>
-            </div>
-            <div className="nav-container">
-            <div className="links">
-                <i class=" "></i>
-                <button className="submit" type="submit">Submit</button>
-            </div>
-                <div className="search-container">
-                    <input type="text" placeholder="Search..." />
-                </div>
-            </div>
+function Navbar() {
+  return (
+    <div className="nav-bar">
+      <div className="aastu-logo">
+        <img
+          src={require("../image/aastu-logo.jpg")}
+          width="200px"
+          height="200px"
+          alt="aastu-logo"
+        />
+        <p>Addis Ababa Science and Technology University</p>
+      </div>
+      <div className="nav-container">
+        <div className="links">
+          <i class=" "></i>
+          <button className="submit" type="submit">
+            Submit
+          </button>
         </div>
-    );
+        <div className="search-container">
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Navbar;
